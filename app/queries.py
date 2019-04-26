b0a182a84b173b5bdbe62e7019f734a8370deba8
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-26 06:32:51 +0000

    Add query

--- app/queries.py
+++ app/queries.py
@@ -4,4 +4,20 @@ from esperlib.queries import register_query
 
 @register_query("All videos")
 def all_videos():
-    return Video.objects.all()
+    from app.models import Video
+    from vgrid import VideoVBlocksBuilder, VideoTrackBuilder, VideoMetadata
+    from rekall import IntervalSet, Interval, Bounds3D, IntervalSetMapping
+
+    videos = Video.objects.all()
+
+    json = VideoVBlocksBuilder() \
+        .add_track(VideoTrackBuilder(
+            'videos', IntervalSetMapping({v.id: IntervalSet([Interval(Bounds3D(0, v.duration()))])
+                                          for v in videos}))) \
+        .add_video_metadata(
+            '/system_media', [VideoMetadata(**{k: getattr(v, k)
+                                               for k in ['id', 'path', 'num_frames', 'fps', 'width', 'height']})
+                              for v in videos]) \
+        .build()
+
+    return json
